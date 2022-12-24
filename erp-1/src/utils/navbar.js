828098e9379cc9c3b7c0852fb40da6d9ff6fa5e8
Soliha <solyatursuntasheva@gmail.com>
2022-12-24 19:04:50 +0500

    add cursor to filter text

--- erp-1/src/utils/navbar.js
+++ erp-1/src/utils/navbar.js
@@ -11,16 +11,21 @@ import OneC from "../assets/images/1c.svg"
 
 export const sidebars = {
   sidebarsPage: [
-    { id: 1, icon: Plus, title: 'Создать (2)', to: '/second' },
-    { id: 2, icon: BottomArrow, title: 'Входящие (2)', to: '' },
-    { id: 3, icon: TopArrow, title: 'Исходящие (2)', to: '' },
-    { id: 4, icon: Saving, title: 'Сохраненные (0)', to: '' },
-    { id: 5, icon: Deleting, title: 'Сохраненные (0)', to: '' },
+    { id: 1, icon: Plus, title: "Создать (2)", to: "/second" },
+    { id: 2, icon: BottomArrow, title: "Входящие (2)", to: "" },
+    { id: 3, icon: TopArrow, title: "Исходящие (2)", to: "" },
+    { id: 4, icon: Saving, title: "Сохраненные (0)", to: "" },
+    { id: 5, icon: Deleting, title: "Сохраненные (0)", to: "" },
   ],
   sidebarsPage2: [
-    { id: 6, icon: Support, title: 'Тех поддержка', to: '' },
-    { id: 7, icon: Settings, title: 'Настройки', to: '' },
-    { id: 8, icon: Logout, title: 'Выйти', to: '' },
-    { id: 9, icon: OneC, to: '' },
-  ]
-}
+    {
+      id: 6,
+      icon: Support,
+      title: `Тех поддержка: +998 99 999 99 99`,
+      to: "",
+    },
+    { id: 7, icon: Settings, title: "Настройки", to: "" },
+    { id: 8, icon: Logout, title: "Выйти", to: "" },
+    { id: 9, icon: OneC, to: "" },
+  ],
+};
